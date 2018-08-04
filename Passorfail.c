46ca6403b9242212b8b7a69ecbfab069382e80bd
aditya0072001 <41997646+aditya0072001@users.noreply.github.com>
2018-08-04 21:09:10 +0530

    Add files via upload

---
+++ Passorfail.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main()
+{
+  int n1,n2=20;
+  printf("Enter your score",&n1);
+  scanf("%d",&n1);
+  /* This program decides your future */
+
+  if(n1>=n2)
+  printf("Congratulation you passed");
+
+  else
+  printf("Better luck next time");
+
+}
---
+++ Sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int n1,n2,s;
+  printf("Enter two numbers");
+  scanf("%d%d",&n1,&n2);
+  s=n1+n2;
+  printf("\nSum=%d",s);
+  getch();
+
+}

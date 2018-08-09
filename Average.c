7a85d41eb036d63801ae0e816915aee228032811
aditya0072001 <41997646+aditya0072001@users.noreply.github.com>
2018-08-09 06:29:52 +0530

    Add files via upload

---
+++ Average.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+  int n1,n2,n3,a;
+
+do
+{
+  printf("Enter three numbers");
+
+}
+while(scanf("%d %d %d")!=3);
+a=n1+n2+n3/3;
+
+printf("Average is %d",&a);
+
+getch();
+
+
+
+}

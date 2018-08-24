6677bab4f22aad73fb4fe0f2d72b42b17cae3495
Ne <41997646+aditya0072001@users.noreply.github.com>
2018-08-24 09:17:37 -0400

    Update SumMultiple.cpp

--- SumMultiple.cpp
+++ SumMultiple.cpp
@@ -5,7 +5,7 @@ int main()
   int n1,n2,s,counter=0,total=3;
   while(counter<total)
   {
-    cout<<"Iteration %d of %d"<<counter+1<<total;
+    cout<<"Iteration of"<<counter+1<<total;
     cout<<"Enter two numbers:\t";
     cin>>n1>>n2;
     s=n1+n2;

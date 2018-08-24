80c465465b86f84c6f814733bc8d8e759c5cf40a
aditya <aditya@DESKTOP-OEF42LI>
2018-08-24 18:18:30 +0530

    some problem with increnation

---
+++ SumMultiple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int n1,n2,s,counter=0,total=3;
+  while(counter<total)
+  {
+    cout<<"Iteration %d of %d"<<counter+1<<total;
+    cout<<"Enter two numbers:\t";
+    cin>>n1>>n2;
+    s=n1+n2;
+    cout<<"\nSum=\n"<<s;
+    counter=counter+1;  //increment
+  }
+
+//  getch();
+
+}

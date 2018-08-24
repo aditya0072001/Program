4f58c02d8994e3c780fe58059c779efdd8a39e49
aditya <aditya@DESKTOP-OEF42LI>
2018-08-24 18:06:19 +0530

    Eclipse main error

---
+++ Passorfail.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int n1,n2=20;
+int main()
+{
+  int n1,n2=20;
+  cout<<"Enter your score";
+  cin>>n1;
+  /* This program decides your future */
+
+  if(n1>=n2)
+  {
+	  cout<<"Congratulation you passed";
+  }
+
+  else
+  {
+	  cout<<"Better luck next time";
+  }
+
+}

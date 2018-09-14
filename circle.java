1c161ed9af091f321a4dc9e8dd864680a7ab8ca1
Ne <41997646+aditya0072001@users.noreply.github.com>
2018-09-14 19:59:00 +0530

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,21 @@
+package school;
+
+public class circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    int i, f = 1, num;
+		
+	    num=5;
+	   
+	  for(i=1;i<=num;i++)
+	  {
+		  f=f*i;
+			System.out.println("Concatenated String is: "+f);
+
+		  
+	  }
+	}
+
+}
---
+++ swap.java
@@ -0,0 +1,19 @@
+package school;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		  int a=5,b=10;
+
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	System.out.println("After swap : "+a+b);
+
+		
+	}
+
+}
